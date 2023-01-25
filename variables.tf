0555cf90b30bb57d1364af95bf31871c43836cfe
DHayes23 <D.Hayes23@hotmail.com>
2023-01-25 16:28:32 +0000

    Add resources and variables to provision

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "eu-west-1"
+}
+
+variable "key_name" {
+  description = " SSH keys to connect to ec2 instance"
+  default     =  "PrimaryKeyPair"
+}
+
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     =  "t2.medium"
+}
+
+variable "security_group" {
+  description = "Name of security group"
+  default     = "petclinic_security_group"
+}
+
+variable "tag_name" {
+  description = "Tag Name of for Ec2 instance"
+  default     = "my-ec2-instance"
+}
+
+variable "ami_id" {
+  description = "AMI for Ubuntu Ec2 instance 22.04"
+  default     = "ami-0333305f9719618c7"
+}
