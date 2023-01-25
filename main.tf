efdf63f431f11534fb8d6b167f4bd15cb08baa60
DHayes23 <D.Hayes23@hotmail.com>
2023-01-25 19:18:18 +0000

    Remove erroneous security group reference

--- main.tf
+++ main.tf
@@ -30,7 +30,6 @@ resource "aws_security_group" "petclinic_security_group" {
 # Create a VPC to contain the other resources.
 resource "aws_vpc" "MainVPC" {
   cidr_block = "10.0.0.0/16"
-  security_groups= [var.security_group]
 
   tags = {
     Name = "main-vpc"
